e7577767431e9c4204aa87ea1e7c3048d15e679e
Namanskshetty <64603506+Namanskshetty@users.noreply.github.com>
2021-06-05 11:56:54 +0530

    Add files via upload

---
+++ bin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "
+ _  _  ____  __     ___  __   _  _  ____
+/ )( \(  __)(  )   / __)/  \ ( \/ )(  __)
+\ /\ / ) _) / (_/\( (__(  O )/ \/ \ ) _)
+(_/\_)(____)\____/ \___)\__/ \_)(_/(____)
+"
+pip install PyOpenGL
+pip install glfw
+pip install PyOpenGL
+pip install Pillow
+echo "DONE INSTALLING REQUIREMENTS(Wait for 3 seconds to start)"
+sleep 1
+echo "3"
+sleep 1
+echo "2"
+sleep 1
+echo "1"
