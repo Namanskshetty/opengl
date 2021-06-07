05f28ee3d05cf70a4f92f1de8251b41af509d2b4
Namanskshetty <64603506+Namanskshetty@users.noreply.github.com>
2021-06-07 12:07:19 +0530

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-#!/usr/bin/python3
 echo "
  _  _  ____  __     ___  __   _  _  ____ 
 / )( \(  __)(  )   / __)/  \ ( \/ )(  __)
 \ /\ / ) _) / (_/\( (__(  O )/ \/ \ ) _) 
 (_/\_)(____)\____/ \___)\__/ \_)(_/(____)
 "
-pip install PyOpenGL
-pip install glfw
-pip install PyOpenGL
-pip install Pillow
-pip install numpy
+sh ./script.sh
 echo "DONE INSTALLING REQUIREMENTS(Wait for 3 seconds to start)"
 sleep 1
 echo "3"
