7ff5dd8f92faef0488f5079d0c06ff6f9efce12e
Namanskshetty <64603506+Namanskshetty@users.noreply.github.com>
2021-06-07 11:17:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,12 @@ import subprocess
 subprocess.call("./bin.sh", shell=True)
 import glfw
 from OpenGL.GL import *
+import numpy as np
+from math import sin, cos
 
 # initializing glfw library
 if not glfw.init():
     raise Exception("glfw can not be initialized!")
-
-# Configure the OpenGL context.
-# If we are planning to use anything above 2.1 we must at least
-# request a 3.3 core context to make this work across platforms.
-glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
-glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
-glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
-glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, True)
-# 4 MSAA is a good default with wide support
-glfw.window_hint(glfw.SAMPLES, 4)
 
 # creating the window(width 720 * 480)
 window = glfw.create_window(720, 480, "Naman", None, None)
@@ -35,10 +27,36 @@ glfw.set_window_pos(window, 400, 200)
 # make the context current
 glfw.make_context_current(window)
 
+vertices = [-0.5, -0.5, 0.0,
+             0.5, -0.5, 0.0,
+             0.0,  0.5, 0.0]
+
+colors = [1.0, 0.0, 0.0,
+          0.0, 1.0, 0.0,
+          0.0, 0.0, 1.0]
+
+vertices = np.array(vertices, dtype=np.float32)
+colors = np.array(colors, dtype=np.float32)
+
+glEnableClientState(GL_VERTEX_ARRAY)
+glVertexPointer(3, GL_FLOAT, 0, vertices)
+
+glEnableClientState(GL_COLOR_ARRAY)
+glColorPointer(3, GL_FLOAT, 0, colors)
+glClearColor(0, 0.1, 0.1, 1)#RGB Alpha(color)
+
 
 # the main application loop
 while not glfw.window_should_close(window):
     glfw.poll_events()#use or program will crash
+    glClear(GL_COLOR_BUFFER_BIT)
+    ct = glfw.get_time()  # returns the elapsed time, since init was called
+
+    glLoadIdentity()
+    glScale(abs(sin(ct)), abs(sin(ct)), 1)
+    glRotatef(sin(ct) * 50, 0, 0, 1)
+    glTranslatef(sin(ct), cos(ct), 0)
+    glDrawArrays(GL_TRIANGLES, 0, 3)
     glfw.swap_buffers(window)
 
 # terminate glfw, free up allocated resources
